6dc5671f5b9fb7aa13db38573eee661aa6f0a764
pradeeprawatnext <pradeeprawatnext@gmail.com>
2025-03-10 10:34:46 +0530

    10th march Lab001 added

---
+++ src/ex_01_Java_Basics/Lab_001_HelloWorldProgram.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+import java.sql.SQLOutput;
+
+public class Lab_001_HelloWorldProgram {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
